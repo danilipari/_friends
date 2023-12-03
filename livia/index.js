8185570d46ac966133bfdc761616a51b058059c2
Dani Lipari <lipari.assistenza@gmail.com>
2023-12-03 18:25:44 +0100

    chore: lint

--- livia/index.js
+++ livia/index.js
@@ -19,7 +19,7 @@ app.use(
 
 app.use(express.static('./livia/html/'));
 
-app.get('/livia/*', (req, res) =>
+app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: './livia/html/'}),
 );
 

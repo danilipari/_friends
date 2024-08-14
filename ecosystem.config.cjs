5920f3c1fb22370b9444bd088471871437b52fc4
Dani Lipari <dani.lipari@dontouch.ch>
2024-08-14 03:25:11 +0200

    chore: revert

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,22 @@
 module.exports = {
   apps: [
-    {
-      name: 'main-app',
-      script: 'sh',
-      args: '-c "node translate.mjs && node index.js"',
-      node_args: '--experimental-modules',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-      },
-      env_development: {
-        NODE_ENV: 'development',
-        PROJECT: 'livia',
-      },
-    },
+    // {
+    //   name: 'main-app',
+    //   script: 'sh',
+    //   args: '-c "node translate.mjs && node index.js"',
+    //   node_args: '--experimental-modules',
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   max_memory_restart: '1G',
+    //   env: {
+    //     NODE_ENV: 'production',
+    //   },
+    //   env_development: {
+    //     NODE_ENV: 'development',
+    //     PROJECT: 'livia',
+    //   },
+    // },
     {
       name: 'twilio-app',
       script: 'sh',
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const { middlewareReadFiles } = require('./middlewares.cjs');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const { middlewareReadFiles } = require("./middlewares.cjs");
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const app = express();
 app.use(cors());
@@ -15,42 +15,52 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      'img-src': ['\'self\'', 'https: data:'],
-      'script-src': ['\'self\'', 'https: data:'],
-      'default-src': ['\'self\'', 'https: data:'],
+      "img-src": ["'self'", "https: data:"],
+      "script-src": ["'self'", "https: data:"],
+      "default-src": ["'self'", "https: data:"],
     },
-  }),
+  })
 );
 
 // Twilio project
-const twilioPath = '/twilio';
-const twilioXmlPath = './twilio/';
+const twilioPath = "/twilio";
+const twilioXmlPath = "./twilio/";
 
 app.use(twilioPath, express.static(twilioXmlPath));
-app.get(`${twilioPath}/:lang`, middlewareReadFiles(twilioXmlPath), (req, res) => {
-  const lang = req.params.lang || 'it';
-  const fileName = `index-${lang}.xml`;
-  res.sendFile(fileName, {root: twilioXmlPath});
-});
+app.post(
+  `${twilioPath}/:lang`,
+  middlewareReadFiles(twilioXmlPath),
+  (req, res) => {
+    const lang = req.params.lang || "it";
+    const fileName = `index-${lang}.xml`;
+    res.sendFile(fileName, { root: twilioXmlPath });
+  }
+);
 
 // Livia project
-const liviaPath = '/livia';
-const liviaHtmlPath = isProd ? `.${liviaPath}/html/` : `.${liviaPath}/html-dev/`;
-const liviaTranslatePath = `.${liviaPath}/translate/`;
+// const liviaPath = "/livia";
+// const liviaHtmlPath = isProd
+//   ? `.${liviaPath}/html/`
+//   : `.${liviaPath}/html-dev/`;
+// const liviaTranslatePath = `.${liviaPath}/translate/`;
 
-app.use(liviaPath, express.static(liviaHtmlPath), express.static(liviaTranslatePath));
-app.get(`${liviaPath}/*`, middlewareReadFiles(liviaHtmlPath), (req, res) => {
-  const requestedPath = `${liviaPath}${req.path}`;
-  const canAccess = (res.locals.readedFiles || [])?.includes(requestedPath);
-  if (canAccess) {
-    res.status(200).sendFile('index.html', { root: liviaHtmlPath });
-  } else {
-    res.status(404).sendFile('404.html', { root: liviaHtmlPath });
-  }
-});
+// app.use(
+//   liviaPath,
+//   express.static(liviaHtmlPath),
+//   express.static(liviaTranslatePath)
+// );
+// app.get(`${liviaPath}/*`, middlewareReadFiles(liviaHtmlPath), (req, res) => {
+//   const requestedPath = `${liviaPath}${req.path}`;
+//   const canAccess = (res.locals.readedFiles || [])?.includes(requestedPath);
+//   if (canAccess) {
+//     res.status(200).sendFile("index.html", { root: liviaHtmlPath });
+//   } else {
+//     res.status(404).sendFile("404.html", { root: liviaHtmlPath });
+//   }
+// });
 
 // Agnese
-app.get('/agnese', (req, res) => {
+app.get("/agnese", (req, res) => {
   res.send(`
     <iframe src="https://www.edusogno.com" width="100%" height="100%">
       <p>Il tuo browser non supporta gli iframe.</p>
@@ -59,10 +69,10 @@ app.get('/agnese', (req, res) => {
 });
 
 // Default route
-app.get('/', (req, res) =>
-  res.send(`<h3>Welcome!</h3>`),
-);
+app.get("/", (req, res) => res.send(`<h3>Welcome!</h3>`));
 
 // Start the app by listening on the default Heroku port
 const port = process.env.PORT || 8000;
-app.listen(port, () => { console.log(`Project is running on port ${port}.`) });
+app.listen(port, () => {
+  console.log(`Project is running on port ${port}.`);
+});

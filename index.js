7b4ca594bf4f54d4914f8d87745caa93c7600db9
Dani Lipari <dani.lipari@dontouch.ch>
2025-02-12 02:53:19 +0100

    feat(spa): init fr

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      "img-src": ["'self'", "https: data:"],
-      "script-src": ["'self'", "https: data:"],
-      "default-src": ["'self'", "https: data:"],
+      "default-src": ["'self'", "https:"],
+      "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https:", "data:"],
+      "img-src": ["'self'", "https:", "data:"],
     },
   })
 );

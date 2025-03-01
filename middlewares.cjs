8b3bc856d9cde32ca7594b3f99f9c29975afbbfc
Dani Lipari <dani.lipari@dontouch.ch>
2025-03-01 23:02:18 +0100

    feat: refactor middleware to read files recursively from a directory

--- middlewares.cjs
+++ middlewares.cjs
@@ -1,17 +1,39 @@
 const fs = require('fs');
+const path = require('path');
 
-const middlewareReadFiles = (folderPath) => (req, res, next) => {
-  fs.readdir(folderPath, (err, files) => {
-    if (err) {
-      console.debug("Errore nella lettura della cartella:", err);
-      res.locals.readedFiles = [];
+/**
+ * Middleware per leggere i file in una directory
+ * @param {string} dirPath - Percorso della directory da leggere
+ * @returns {Function} Middleware Express
+ */
+const middlewareReadFiles = (dirPath) => {
+  return (req, res, next) => {
+    try {
+      const files = [];
+      const readDir = (dir) => {
+        const items = fs.readdirSync(dir);
+        items.forEach(item => {
+          const itemPath = path.join(dir, item);
+          const stat = fs.statSync(itemPath);
+          if (stat.isDirectory()) {
+            readDir(itemPath);
+          } else {
+            const relativePath = itemPath.replace(dirPath, '');
+            files.push(relativePath);
+          }
+        });
+      };
+      
+      readDir(dirPath);
+      res.locals.readedFiles = files;
+      next();
+    } catch (error) {
+      console.error('Errore nella lettura dei file:', error);
+      next(error);
     }
-
-    res.locals.readedFiles = files;
-    next();
-  });
+  };
 };
 
 module.exports = {
-  middlewareReadFiles,
+  middlewareReadFiles
 };
